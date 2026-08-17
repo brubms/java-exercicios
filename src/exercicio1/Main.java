98576311742152b7d9cb5c80acbd829a850448bb
brubms <brunoberna21@gmail.com>
2026-08-17 20:49:03 -0300

    Exercicio 1

---
+++ src/exercicio1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Calculadora calc = new Calculadora();
+        int resultado;
+
+        resultado = calc.somar(10, 20);
+
+        System.out.println(resultado);
+    }
+
+}
