c4eaf18c985c344df8075f6fc494f9fc50ff7fcf
brubms <brunoberna21@gmail.com>
2026-08-18 20:56:29 -0300

    Exercicios de Wrappers e atualização do README.md

--- src/escopodevariaveis/ExemploEscopo.java
+++ src/escopodevariaveis/ExemploEscopo.java
@@ -5,12 +5,12 @@ public class ExemploEscopo {
     private int numero = 10;
 
     public void mostrarNumero(){
-        this.numero = 15;
+        int numeroteste =15;
         System.out.println(numero);
     }
 
     public void tentarAcessarLocal(){
-        System.out.println(numero);
+        System.out.println();
     }
 
 }
---
+++ src/wrappers/Conversor.java
@@ -0,0 +1,20 @@
+package wrappers;
+
+public class Conversor {
+
+    public int converterParaInt(String texto){
+        return Integer.parseInt(texto);
+    }
+
+    public double converterParaDouble(String texto){
+        return Double.parseDouble(texto);
+    }
+
+    public String converterParaStringInt(int numero){
+        return String.valueOf(numero);
+    }
+    public String converterParaStringDouble(int numero){
+        return String.valueOf(numero);
+    }
+
+}
